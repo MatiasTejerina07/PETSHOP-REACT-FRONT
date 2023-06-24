3379d174dd5d9febf9c18e071cf7504a1a2b8e30
Matias Tejerina <127890622+MatiasTejerina07@users.noreply.github.com>
2023-06-24 20:17:25 -0300

    filtros por categoria, falta el filtro que muestre todo (#3)

--- src/pages/Jugueteria.jsx
+++ src/pages/Jugueteria.jsx
@@ -4,19 +4,63 @@ import axios from "axios"
 import { useEffect, useState } from "react"
 
 export default function Jugueteria() {
-    const [products, setProducts] = useState([])
+    const [farmacia, setProduct] = useState([])
+    const [filtros, setFiltros] = useState({
+        farmacia: false,
+        jugueteria: false
+    })
+
+    const products = async () => {
+        try {
+            const res = await axios('https://mindhub-xj03.onrender.com/api/petshop');
+            setProduct(res.data);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    const mostrarFiltros = () => {
+        if (filtros.farmacia === true) {
+            setProduct(farmacia.filter((product) => product.categoria != "jugueteria"))
+        } else if (filtros.jugueteria === true) {
+            setProduct(farmacia.filter((product) => product.categoria != "farmacia"))
+        }
+        else {
+            products()
+        }
+    }
+    console.log(filtros);
+
     useEffect(
         () => {
-            axios('https://mindhub-xj03.onrender.com/api/petshop')
-                .then(res => setProducts(res.data))
+
+            products()
         }, []
     )
-    console.log(products);
+    useEffect(() => {
+        mostrarFiltros();
+    }, [filtros])
+
+
     return (
         <div className="w-full h-full">
             <Categorias />
+            <div className="w-full flex justify-around items-center pt-8">
+                <label htmlFor="farmacia">Farmacia <input onChange={(e) => setFiltros({ ...filtros, farmacia: e.target.checked })} type="checkbox" name="farmacia" id="farmacia" />
+                </label>
+                <label htmlFor=""> Jugueteria <input onChange={(e) => setFiltros({ ...filtros, jugueteria: e.target.checked })} type="checkbox" name="jugueteria" id="jugueteria" />
+                </label>
+                <label htmlFor=""> Todos <input type="checkbox" name="" id="" />
+                </label>
+                <select className="w-52 h-10" name="filter" id="0">
+                    <option value="1">Precio de mayor a menor</option>
+                    <option value="1">Precio de menor a mayor</option>
+                    <option value="2">Nombre de a--Z</option>
+                    <option value="3">Nombre de Z--a</option>
+                </select>
+            </div>
             <div className="w-full flex flex-wrap relative -z-0 justify-center gap-x-10 gap-y-10 pb-10 pt-10">
-                {products && products.map((product) =>
+                {farmacia && farmacia?.map((product) =>
                     <Example key={product._id} category={product?.categoria} title={product?.producto} image={product?.imagen} price={product?.precio} quantity={product?.disponibles} />
                 )}
             </div>
