e2fdc0d3e5475f477904b4bcd62dabdd2f60f491
Mat Tejerina <matiastejerina94@gmail.com>
2023-06-28 14:19:07 -0300

    cart

--- src/context/userContext.jsx
+++ src/context/userContext.jsx
@@ -3,9 +3,20 @@ import { createContext, useState, useContext } from "react";
 export const UserContext = createContext()
 
 const userProvider = ({ children }) => {
-    const [user, setUser] = useState(false)
+    const [user, setUser] = useState(
+        {
+            isActive: false,
+            id: 1233,
+            photo: '',
+            role: ""
+        })
+    const [cart, setCart] = useState({
+        id: user.id,
+        items: []
+    })
+
     return (
-        <UserContext.Provider value={{ user, setUser }}>
+        <UserContext.Provider value={{ user, setUser, cart, setCart }}>
             {children}
         </UserContext.Provider>
     )
